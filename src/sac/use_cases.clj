bafdff14053846541a3625f2393a27440ac6bd5e
Nikita Mistiukov <nekifirus@gmail.com>
2022-12-25 12:09:56 +0300

    add two commands
    Application will have other functions but only this two changes
    data. App can't being compiled right now. Will fixed in next step.

---
+++ src/sac/use_cases.clj
@@ -0,0 +1,17 @@
+(ns sac.use-cases
+  "We have only two processes changes data. Application will have
+  another functions. But CQRS is the best")
+
+(defn register
+  "Stores a new Customer in a repository and authenticate him"
+  [email name password customer-repo access-control]
+  (when (nil? (get-customer customer-repo email))
+    (make-customer customer-repo email name password)
+    (authenticate access-control email password customer-repo)))
+
+(defn search-articles
+  "Search articles in an external knowledge base and store them into an internal storage"
+  [words knowledge-base articles-repo]
+  (->> words
+       (get-articles knowledge-base)
+       (save-articles articles-repo)))
