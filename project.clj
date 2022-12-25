72dcb2ce675c26f08f87ce18566dc6aa703cc578
Nikita Mistiukov <nekifirus@gmail.com>
2022-12-25 11:48:39 +0300

    initial commit
    Right now this is empty Leiningen project. I will grow it.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject sac "0.1.0-SNAPSHOT"
+  :description "Science articles cash. Exam task to join Clojure developers team"
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :repl-options {:init-ns sac.core})
---
+++ src/sac/core.clj
@@ -0,0 +1,2 @@
+(ns sac.core)
+
