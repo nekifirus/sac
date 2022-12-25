b9e2107d5cc65f122e2cbac2851529cac3ae5d92
Nikita Mistiukov <nekifirus@gmail.com>
2022-12-25 11:56:07 +0300

    There was Article and Customer at the begining
    I see the goal of this app to help Customers in searching
    Articles. Without these two entities application will not work.

--- src/sac/core.clj
+++ src/sac/core.clj
@@ -1,2 +1,8 @@
-(ns sac.core)
+(ns sac.core
+  "Domain entities and auto generated functions for SAC app
 
+  Article is a data we need to cache for our Customer")
+
+(defrecord Article [title author date doi])
+
+(defrecord Customer [email name])
